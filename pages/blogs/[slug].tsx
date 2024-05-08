feece8539a398a0815a9430d6ba23f63b0c8a974
LinguiniThePasta <lfh20130915@gmail.com>
2024-05-07 21:19:53 -0400

    Finally figured out how to make a proper slug and how the navigation of a next.js app works

---
+++ pages/blogs/[slug].tsx
@@ -0,0 +1,15 @@
+import {GetServerSideProps, GetServerSidePropsContext} from "next";
+import { useRouter } from "next/router";
+
+const blogPage = ({slug}) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps<Props, Params> = async ({
+
+})
+
+export default blogPage;
